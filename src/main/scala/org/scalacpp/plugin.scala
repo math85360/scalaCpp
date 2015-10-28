ab843aa669b16f0f838396a323f31b1a0cad7eb3
Mathieu Leguey <mathieu.leguey@gmail.com>
2015-10-28 15:12:45 +0100

    .

--- src/main/scala/org/scalacpp/plugin.scala
+++ src/main/scala/org/scalacpp/plugin.scala
@@ -51,67 +51,94 @@ class ScalaCppComponent(val global: Global) extends PluginComponent with TypingT
           lst.map(parseTree(_)(parameters.copy(_packageName = ref))).mkString("\n")
         case ClassDef(modifiers, tpeName, tparams, impl) =>
           //System.err.println("class "+tpeName)
-          impl.body.map(parseTree(_)(parameters.copy(_clsName = tpeName, _indent = 1))).mkString("\n")
+          var visited = Seq[Tree]()
+          // visit each field val/var and add it to visited
+          // next do all impl
+          //"[class] " + impl.body.map(parseTree(_)(parameters.copy(_clsName = tpeName, _indent = 1))).mkString("\n")
+          ""
         case ModuleDef(modifiers, tpeName, impl) =>
           //System.err.println("module "+tpeName)
-          impl.body.map(parseTree(_)(parameters.copy(_clsName = tpeName, _indent = 1))).mkString("\n")
+          /*val map = impl.collect({
+            case ValDef(mods, name, tpt, rhs) => (name.toString.trim, name.toString.trim + "_")
+          }).toMap
+          "[module] " + impl.body.map(parseTree(_)(parameters.copy(_clsName = tpeName, _indent = 1, _variables =
+          map))).mkString("\n")*/
+          impl.body.collect({
+            case ValDef(mods, name, tpt, rhs) => (name.toString.trim, name.toString.trim + "_")
+          }).mkString(";\n")
         case q"$mods def $name[..$tparams](...$vparams):$tpt = $rhs" => //DefDef(mods, name, tparams, vparams, tpt, rhs) =>
           //System.err.println(parameters.toString + "_" + name)
           //System.err.println(tparams.toString)
           //System.err.println(vparams.toString)
-          applyModifiers(mods) + cppType(tpt.toString) + " " + parameters.methodName(name.toString) + "(" + convertArgList(vparams) + ") {\n" +
+          val map = vparams.flatten.map(x => x.name.toString).map(x => (x.trim(), x.trim() + "_")).toMap
+          val internalVar = parameters.copy(_variables = map)
+          "[method] " + applyModifiers(mods) + getTypeFromScala(tpt).cppType + " " + parameters.methodName(name.toString) + "(" + convertArgList(vparams)(internalVar) + ") {\n" +
             (rhs match {
-              case _ if tpt.toString.trim() equals "Unit" => parseTree(rhs)(parameters.copy(_indent = 1)) + ";" + rhs.tpe.toString + tpt.toString
-              case Block(lst, tpe) => lst.map(parseTree(_)(parameters.copy(_indent = 1))).mkString(";\n") + "\nreturn " + parseTree(tpe)(parameters.copy(_indent = 1))
-              case _ => "return " + parseTree(rhs)(parameters.copy(_indent = 1)) + ";"
+              case Literal(Constant(())) => "(empty)"
+              case _ if tpt.toString.trim() equals "Unit" => "(void) " + parseTree(rhs)(internalVar.copy(_indent = 1)) + ";"
+              case Block(lst, tpe) => "(block) " + lst.map(parseTree(_)(internalVar.copy(_indent = 1))).mkString(";\n") + "\nreturn " + parseTree(tpe)(internalVar.copy(_indent = 1))
+              case _ => "(return) return " + parseTree(rhs)(internalVar.copy(_indent = 1)) + ";"
               //case Block(lst, tpe) => lst.map(parseTree(_)(parameters.copy(_indent = 1))).mkString("\n") + "\nreturn " + parseTree(tpe)(parameters.copy(_indent = 1))
               //case _ => "****"+rhs.getClass.toString
               //case _ => parseTree(rhs)
             }) + "\n}"
-        case Literal(Constant(())) => ""
-        case Literal(Constant(x)) => f"$x"
-        case Select(tree, name) =>
-          f"$name"
-        case Block(lst, tpe) =>
-          lst.map(parseTree(_)(parameters.copy(_indent = 1))).mkString("\n") + "\n" + tpe.toString
+        case Literal(Constant(())) => "[4] "
+        case Literal(Constant(x)) => f"[5] $x"
+        //case x: Select if parameters.variables.contains(x.toString.trim) => f"[6a] ${getMethod(x)}"
+        case x: Select => f"[6] ${getMethod(x)}"
+        case Block(lst, tpe) => "[7] " + lst.map(parseTree(_)(parameters.copy(_indent = 1))).mkString("\n") + "\n" + parseTree(tpe)
+        //tpe.toString
         case q"$mods val $name: $tpt = $rhs" =>
-          f"${cppType(tpt.toString)} $name = ${parseTree(rhs)};"
+          f"[8] ${getTypeFromScala(tpt).cppType} ${parameters.getVariableName(name)} = ${parseTree(rhs)};"
         case q"$mods var $name: $tpt = $rhs" =>
-          f"${cppType(tpt.toString)} $name = ${parseTree(rhs)};"
-        case Literal(x) =>
-          f"$x"
-        case q"$tpname.this" if tpname.toString == "App" => ""
-        case q"$tpname.this" => tpname.toString()
-        case q"$lhs = $rhs" => "${parseTree(lhs)} =  ${parseTree(lhs)}"
-
+          if (rhs.isEmpty) {
+            val r = tpt.toString match {
+              case "Long" => 0
+              case "String" => "\"\""
+            }
+            f"${getTypeFromScala(tpt).cppType} ${parameters.getVariableName(name)} = $r;"
+          } else {
+            f"${getTypeFromScala(tpt).cppType} ${parameters.getVariableName(name)} = ${parseTree(rhs)};"
+          }
+        case Literal(x) => f"[A] $x"
+        case q"$tpname.this" if tpname.toString == "App" => "[B]"
+        case q"$tpname.this" => f"[C] $tpname"
+        case q"$lhs = $rhs" => f"[D] ${parseTree(lhs)}=${parseTree(rhs)}"
         case q"if ($cond) $lhs else $rhs" =>
-          f"""if(${parseTree(cond)}) {
+          f"""[E] if(${parseTree(cond)}) {
             ${parseTree(lhs)};
-            }else{
+            [E]}else{
             ${parseTree(rhs)};
             }""".stripMargin
         /*"if(" + parseTree(cond)(parameters.copy(_indent = 1)) + ") {\n" +
             parseTree(lhs)(parameters.copy(_indent = 1)) + ";\n" +
             "}else{\n" +
             parseTree(rhs)(parameters.copy(_indent = 1)) + ";\n}"*/
-        case q"$method(...$args)" =>
-          f"${method.getClass.toString}${method.toString()}(${convertArgsCall(args)})"
+        case q"$method(...$args)" if parameters.variables.contains(method.toString.trim) => f"${parameters.variables(method.toString.trim)}"
+        case q"$method(...$args)" => f"[F] ${method.toString}(${convertArgsCall(args)})"
         case x =>
-          "!!!" + (x.getClass.toString) + " " +
-            (x.toString)
+          "[H] !!!" + (x.getClass.toString) + " " + (x.toString)
       }
     }
-    def convertArgList(lst: List[List[ValDef]]) = {
-      lst.flatten.map(x => cppType(x.tpt.toString) + " " + x.name.toString).mkString(", ")
+
+    def getMethod(v: Tree)(implicit parameters: Parameters): String = {
+      v match {
+        case Select(qualifier, name) if qualifier.toString().endsWith(".this") => parameters.variables.getOrElse(name.toString.trim, f"$name")
+        case Select(qualifier, name) => f"$qualifier.$name"
+        case Ident(name) => f"${parameters.variables.getOrElse(name.toString.trim, name.toString.trim)}"
+      }
+    }
+
+    def convertArgList(lst: List[List[ValDef]])(implicit parameters: Parameters) = {
+      lst.flatten.map(x => getTypeFromScala(x.tpt).cppType + " " + parameters.variables(x.name.toString)).mkString(", ")
     }
     def convertArgsCall(lst: List[List[Tree]]) = {
       lst.flatten.map(x => parseTree(x)).mkString(", ")
     }
     val applyModifiers: PartialFunction[Modifiers, String] = {
-      case x if x.isPrivate => "private "
       case x => ""
     }
-    val cppType: PartialFunction[String, String] = {
+    /*val cppType: PartialFunction[String, String] = {
       case "Int" => "int"
       case "Long" => "long"
       case "Double" => "double"
@@ -119,26 +146,47 @@ class ScalaCppComponent(val global: Global) extends PluginComponent with TypingT
       case "Char" => "char"
       case "Unit" => "void"
       case x => x
-    }
+    }*/
+
+    val scalaToCppTypes = Seq(
+      ScalaToCppType("Int", "int", "0"),
+      ScalaToCppType("Long", "long", "0"),
+      ScalaToCppType("Double", "double", "0.0"),
+      ScalaToCppType("String", "String", "\"\""),
+      ScalaToCppType("Char", "char", "0"),
+      ScalaToCppType("Unit", "void", ""),
+      ScalaToCppType("Int", "int", "0"))
+
+    def getTypeFromScala(scalaType: String): ScalaToCppType = scalaToCppTypes.find(_.scalaType == scalaType).getOrElse(ScalaToCppType(scalaType, scalaType, "null"))
+    def getTypeFromScala(tree: Tree): ScalaToCppType = getTypeFromScala(tree.toString.trim)
   }
+
+  case class Parameters(
+      packageName: Seq[String],
+      clsName: Seq[String],
+      indent: Int = 0,
+      variables: Map[String, String] = Map()) {
+    def copy(
+      _packageName: Option[String] = None,
+      _clsName: Option[String] = None,
+      _indent: Int = 0,
+      _variables: Map[String, String] = Map()) =
+      new Parameters(
+        packageName ++ _packageName,
+        clsName ++ _clsName,
+        indent + _indent,
+        variables ++ _variables)
+
+    def bol = "  " * (indent * 2)
+    def methodName(name: String) = ((Seq[String]() ++ packageName ++ clsName) :+ name).map(_.replace('.', '_')).mkString("_")
+    def getVariableName(name: String): String = variables(name.trim)
+    def getVariableName(name: TermName): String = getVariableName(name.toString)
+    def getVariableName(name: Name): String = getVariableName(name.toString)
+  }
+
 }
 
-case class Parameters(
-    packageName: Seq[String],
-    clsName: Seq[String],
-    indent: Int = 0) {
-  def copy(
-    _packageName: Option[String] = None,
-    _clsName: Option[String] = None,
-    _indent: Int = 0) =
-    new Parameters(
-      packageName ++ _packageName,
-      clsName ++ _clsName,
-      indent + _indent)
-
-  def bol = "  " * (indent * 2)
-  def methodName(name: String) = ((Seq[String]() ++ packageName ++ clsName) :+ name).map(_.replace('.', '_')).mkString("_")
-}
+case class ScalaToCppType(scalaType: String, cppType: String, defaultValue: String)
 
 object Parameters {
 
